f1be44a4298b619bee723a222d1f03ed4f28086e
hazedxz <tumblr1275@gmail.com>
2026-06-13 01:34:55 -0400

    Add ApplyDarkCinematicStyle function declaration
    Added ApplyDarkCinematicStyle function declaration.

--- src/liquid_menu.h
+++ src/liquid_menu.h
@@ -4,18 +4,18 @@
 #include <string>
 #include <vector>
 
-// Forward declaration of our texture loader
+
 GLuint LoadSVGTexture(const char* filepath, int targetWidth, int targetHeight);
 
-// Structure to hold individual weapon data
+
 struct BuyMenuItem {
     std::string displayName;
-    std::string command; // This also matches the .svg filename
+    std::string command; 
     int price;
     GLuint textureID;
 };
 
-// Main functions to hook into the game loop
+
 void InitializeBuyMenu();
 void RenderBuyMenu(bool* pOpen);
 void ApplyDarkCinematicStyle();
